01233e2a2017febb8f1439c559afa2f3e167de3b
Constantin Angheloiu <constantin.angheloiu@gmail.com>
2025-06-09 00:48:27 +0300

    add icons as en embedd component

--- lib/raffley_web/custom_components/custom_components.ex
+++ lib/raffley_web/custom_components/custom_components.ex
@@ -1,19 +1,26 @@
 defmodule RaffleyWeb.CustomComponents do
   use Phoenix.Component
+  use RaffleyWeb, :verified_routes
+
+  embed_templates "icons/*"
 
   attr :status, :atom, required: true, values: [:upcoming, :open, :closed]
   attr :class, :string, default: nil
 
   def badge(assigns) do
     ~H"""
-    <defoverridable(v class={[
+    <div class={[
       "rounded-md px-2 py-1 text-xs font-medium uppercase inline-block border",
       @status == :open && "text-lime-600 border-lime-600",
       @status == :upcoming && "text-amber-600 border-amber-600",
       @status == :closed && "text-gray-600 border-gray-600"
     ]}>
       {@status}
-    </defoverridable(v>
+
+      <.moon_icon />
+
+      <a href={~p"/"}>Home</a>
+    </div>
     """
   end
 end
